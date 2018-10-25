0e1c0594b15564d3db02c4b4df93b73095dd33c8
Tomas Vallejos <tomas.vallejos12@gmail.com>
2018-10-25 15:20:32 -0300

    Siege unit javadoc ready

--- src/Units/SiegeUnit.java
+++ src/Units/SiegeUnit.java
@@ -4,55 +4,106 @@ import Buildings.Castle;
 import Interfaces.Attackable;
 import abstractClasses.Unit;
 
+/**
+ * Siege Unit class
+ * @author Tomas Vallejos
+ */
 public class SiegeUnit extends Unit {
+    /**
+     * variable which represents the siege unit initial health
+     */
     private final static int siegeHP = 400;
+    /**
+     * variable which represents the siege unit attack power
+     */
     private final static int siegeAP = 15;
 
-
+    /**
+     * Siege constructor calls unit constructor
+     */
     public SiegeUnit(){
         super(siegeHP,siegeAP);
     }
 
+    /**
+     * if a Siege attacks the entity reacts as attacked by a Siege Unit
+     * @param Attacked the Object whos attacked
+     */
     public void attack(Attackable Attacked) {
         if (this.isAlive()) {
             Attacked.attackedBySiege(this);
         }
     }
+
+    /**
+     * specific damage and multiplier is applied if Siege is attacked
+     * by Infantry Units
+     * @param IU the Infantry Unit which performs the attack
+     */
     @Override
     public void attackedByInfantry(InfantryUnit IU) {
         int dmg = (int) (1.2*IU.getAP());
         this.getDamagedBy(dmg);
     }
 
+    /**
+     * specific damage and multiplier is applied if Siege is attacked
+     * by Archer Units
+     * @param AU the Archer Unit which performs the attack
+     */
     @Override
     public void attackedByArcher(ArcherUnit AU) {
         int dmg = (int) (0.8*AU.getAP());
         this.getDamagedBy(dmg);
     }
 
+    /**
+     * specific damage and multiplier is applied if Siege is attacked
+     * by Cavalry Units
+     * @param CU the Cavalry Unit which performs the attack
+     */
     @Override
     public void attackedByCavalry(CavalryUnit CU) {
         int dmg = (int) (1.2*CU.getAP());
         this.getDamagedBy(dmg);
     }
 
+    /**
+     * specific damage and multiplier is applied if Siege is attacked
+     * by Siege Units
+     * @param SU The Siege Unit which performs the attack
+     */
     @Override
     public void attackedBySiege(SiegeUnit SU) {
         int dmg = (int) (1.5*SU.getAP());
         this.getDamagedBy(dmg);
     }
 
+    /**
+     * specific healing and multiplier is applied if Siege is repaired
+     * by Villagers
+     * @param V the Villager which performs the attack
+     */
     @Override
     public void attackedByVillager(Villager V) {
         int heal = (int) (0.5*V.getAP());
         this.getHealedBy(heal);
     }
 
+    /**
+     * Siege cannot be healed by monks
+     * @param M The Monk which performs the heal
+     */
     @Override
     public void attackedByMonk(Monk M) {
         //nothing
     }
 
+    /**
+     * specific damage and multiplier is applied if Siege is attacked
+     * by castles
+     * @param castle The Castle which performs the attack
+     */
     @Override
     public void attackedByCastle(Castle castle) {
         int dmg = (int) (0.5*castle.getAP());
